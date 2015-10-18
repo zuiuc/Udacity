062eb5e3077d9172da191199f457f84a7f642c42
Qi <qizhao2@gmail.com>
2015-10-18 00:12:13 -0700

    working in progress

--- front-end developer/p2(resume)/js/resumeBuilder.js
+++ front-end developer/p2(resume)/js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ //$("#main").append("Qi Zhao");
+ //$("#header").append();
+ var name = "Qi Zhao";
+ var role = "Software Engineer";
+ var formattedName = HTMLheaderName.replace("%data%", name);
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+ $("#header").append(formattedName);
+ $("#header").append(formattedRole);
